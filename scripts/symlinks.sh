73772b27eb55bd5200591ffa95bfa12a67b08f4b
rush1016 <russelmicotamayo@gmail.com>
2025-02-26 21:51:48 +0800

    added: more error handling and success message

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -25,6 +25,7 @@ for home_config_dir in $home_config_dirs; do
     for file in $(ls -A $dir/$home_config_dir); do
         mv ~/.$file $old_dir/ 2> /dev/null
         ln -s $dir/.$file ~/.$file 2> /dev/null
+        echo "Linked $file"
     done
 done
 echo "done..."
@@ -32,8 +33,9 @@ echo "done..."
 echo "Creating symlinks for ~/.config files"
 mkdir -p $old_dir/.config
 for config in $config_files; do
-    mv $config_dir/$config $old_dir/.config
-    ln -s $dir/$config $config_dir
+    mv $config_dir/$config $old_dir/.config 2> /dev/null
+    ln -s $dir/$config $config_dir 2> /dev/null
+    echo "Linked $config"
 done
 echo "done..."
 
@@ -41,6 +43,8 @@ echo "Creating symlinks for other bin scripts"
 mkdir -p $user_bin
 mkdir -p $old_dir/.local/bin
 for binary in $(ls $dir/.local/bin); do
-    mv $user_bin/$binary $old_dir/.local/bin/
-    ln -s $dir/.local/bin/$binary $user_bin/$binary
+    mv $user_bin/$binary $old_dir/.local/bin/ 2> /dev/null
+    ln -s $dir/.local/bin/$binary $user_bin/$binary 2> /dev/null
+    echo "Linked $binary"
 done
+echo "done..."
