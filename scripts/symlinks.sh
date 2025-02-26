fe23fdc47da4f7788a23c54cecae45eec346e71b
rush1016 <russelmicotamayo@gmail.com>
2025-02-26 21:34:48 +0800

    added: symlink scripts for new configs

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -7,9 +7,10 @@
 
 dir=~/.dotfiles
 old_dir=~/.dotfiles-old
+user_bin=~/.local/bin
 
 # Files in $HOME
-home_config_files="zshrc tmux.conf"
+home_config_dirs="zsh tmux"
 
 # Files in ~/.config
 config_dir=~/.config
@@ -20,9 +21,11 @@ mkdir -p $old_dir
 cd $dir
 
 echo "Creating symlinks for \$HOME config files"
-for file in $home_config_files; do
-    mv ~/.$file $old_dir/ 2> /dev/null
-    ln -s $dir/.$file ~/.$file 2> /dev/null
+for home_config_dir in $home_config_dirs; do
+    for file in $(ls -A $dir/$home_config_dir); do
+        mv ~/.$file $old_dir/ 2> /dev/null
+        ln -s $dir/.$file ~/.$file 2> /dev/null
+    done
 done
 echo "done..."
 
@@ -33,3 +36,11 @@ for config in $config_files; do
     ln -s $dir/$config $config_dir
 done
 echo "done..."
+
+echo "Creating symlinks for other bin scripts"
+mkdir -p $user_bin
+mkdir -p $old_dir/.local/bin
+for binary in $(ls $dir/.local/bin); do
+    mv $user_bin/$binary $old_dir/.local/bin/
+    ln -s $dir/.local/bin/$binary $user_bin/$binary
+done
