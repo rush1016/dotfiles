c6b091de6527a8b7ae5018c478622a8c3415404b
rush1016 <russelmicotamayo@gmail.com>
2025-04-01 12:19:44 +0800

    added: scripts for searching PR by hash (gh cli required)

---
+++ scripts/search-pull-request-by-hash.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Search Pull Request by hash\nEnter commit hash: "
+read hash
+
+gh pr list --search "$hash" --state merged --json number,title,url,author,createdAt > /tmp/pr_results.json
+
+tmux neww -n pr-results bash -c "
+  jq -r '.[] | \"\(.number): \(.title) (by \(.author.login))\"' /tmp/pr_results.json | 
+  fzf --preview 'echo {1} | tr -d \":\" | xargs -I% jq -r \".[] | select(.number == (% | tonumber))\" /tmp/pr_results.json' \
+      --bind 'enter:execute(echo {1} | tr -d \":\" | xargs -I% jq -r \".[] | select(.number == (% | tonumber)) | .url\" /tmp/pr_results.json | xargs open)'
+"
+
