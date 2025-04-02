271fd73d1fe6458f7d4deb1bc3076f3048f8469b
rush1016 <russelmicotamayo@gmail.com>
2025-04-02 13:21:52 +0800

    updated: npm run script bindings

--- scripts/node-development.sh
+++ scripts/node-development.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
-selected=`echo 'dev build' | tr ' ' '\n' | fzf`
-
-if [[ -z $selected ]]; then
-    exit 0
-fi
-
-tmux neww -n node-${selected} bash -c "npm run ${selected}"
+case $1 in
+    "b") tmux neww -n node-build bash -c "npm run build";;
+    "d") tmux neww -n node-dev bash -c "npm run dev";;
+    "w") tmux neww -n node-dev bash -c "npm run watch";;
+    "s") tmux neww -n node-dev bash -c "npm run start";;
+esac
